1944205a857a6672ec30ebd8a47c207776004bed
Ian Hodgetts <ian.hodgetts@capita.co.uk>
2020-08-14 18:04:29 +0100

    Added Bang (multiple of 7)

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -12,11 +12,20 @@ def checkMultipleOf5(num):
     else:
         return(True)
 
+# Use modulus to check if divisible by 7
+def checkMultipleOf7(num):
+    if num % 7: 
+        return(False)
+    else:
+        return(True)
+
 for num in range(1,101):
     if checkMultipleOf3(num):
         print ("Fizz",end="")
     if checkMultipleOf5(num):
         print ("Buzz",end="")
-    if not checkMultipleOf3(num) and not checkMultipleOf5(num):
+    if checkMultipleOf7(num):
+        print ("Bang",end="")
+    if not checkMultipleOf3(num) and not checkMultipleOf5(num) and not checkMultipleOf7(num):
         print ("num=",num," ",end="")
     print ("")
